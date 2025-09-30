8120c826187aa0317f9683e17165735830f79591
duocnvpd00980 <lytuongdn@gmail.com>
2025-09-30 14:37:49 +0700

    Update login_save_state.py

--- login_save_state.py
+++ login_save_state.py
@@ -1,8 +1,7 @@
-
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as pw:
-    browser = pw.chromium.launch(headless=False)
+    browser = pw.chromium.launch(headless=False)  # mở trình duyệt thật
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://chat.openai.com/")
